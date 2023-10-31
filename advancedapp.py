9ee8df73299f7cc7ebde45b6c96407007e2b981d
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-10-31 09:07:01 -0400

    Update advancedapp.py

--- advancedapp.py
+++ advancedapp.py
@@ -2,20 +2,17 @@ import pandas as pd
 import streamlit as st
 import io
 
-# Function to load data from URL
+# Function to load data
 @st.cache
 def load_data(url):
     return pd.read_csv(url)
 
-# Load datasets
+# Load the main dataset and the details dataset
 df = load_data("https://github.com/rmkenv/ejchat/blob/main/ejtoolmatrix.csv?raw=true")
-df_details = load_data("https://raw.githubusercontent.com/rmkenv/ejchat/main/demographics")  
+df_details = load_data("https://raw.githubusercontent.com/rmkenv/ejchat/main/demographics")
 
-# Dictionary for tool names and their indicators
+# Process and store tool indicators
 tool_indicators = {}
-indicator_details = {}
-
-# Process the main dataframe
 for i in range(2, len(df), 2):  # Start from the third row, skipping 'Metric' and 'Source'
     tool_name = df.iloc[i]['Unnamed: 0'].split(' - ')[0]
     indicators = set()
@@ -23,14 +20,9 @@ for i in range(2, len(df), 2):  # Start from the third row, skipping 'Metric' an
         indicator = df.iloc[i][col]
         if pd.notna(indicator):
             indicators.add(indicator)
-            # Capture indicator details
-            indicator_details[indicator] = {
-                "Indicator": df.iloc[i-1][col],
-                "Category": df.iloc[i-2][col]
-            }
     tool_indicators[tool_name] = indicators
 
-# Function to compare indicators among up to three tools
+# Function to compare tool indicators
 def compare_tool_indicators(tool1, tool2, tool3):
     indicators_tool1 = tool_indicators[tool1]
     indicators_tool2 = tool_indicators[tool2]
@@ -47,43 +39,45 @@ def compare_tool_indicators(tool1, tool2, tool3):
 # Streamlit app setup
 st.title("Environmental Tools Indicator Comparison")
 
-# Dropdown for selecting tools
-tool_list = list(tool_indicators.keys())
-tool1 = st.selectbox("Select the first tool:", tool_list, index=0)
-tool2 = st.selectbox("Select the second tool:", tool_list, index=1)
-tool3 = st.selectbox("Select the third tool (optional):", ["None"] + tool_list, index=0)
+# Dropdowns for tool selection
+tool1 = st.selectbox("Select the first tool:", list(tool_indicators.keys()), index=0)
+tool2 = st.selectbox("Select the second tool:", list(tool_indicators.keys()), index=1)
+tool3 = st.selectbox("Select the third tool (optional):", ["None"] + list(tool_indicators.keys()), index=0)
 
-# Button to compare indicators
+# Button to compare tools
 if st.button("Compare Tools"):
     unique_to_tool1, unique_to_tool2, unique_to_tool3, common_to_all = compare_tool_indicators(tool1, tool2, tool3)
 
-    # Display results
-    st.write(f"Indicators unique to {tool1}:")
-    st.write(unique_to_tool1)
-    st.write(f"Indicators unique to {tool2}:")
-    st.write(unique_to_tool2)
-    if tool3 != "None":
-        st.write(f"Indicators unique to {tool3}:")
-        st.write(unique_to_tool3)
-    st.write("Indicators common to all selected tools:")
-    st.write(common_to_all)
+    # Prepare a DataFrame for displaying results
+    results = {
+        "Indicator": [],
+        "Unique to": [],
+        "Category": [],
+        "Metric": []
+    }
 
-    # Convert results to DataFrame for downloading
-    results = []
-    for tool, indicators in [(tool1, unique_to_tool1), (tool2, unique_to_tool2), (tool3, unique_to_tool3)]:
+    # Function to add details to the results
+    def add_details(indicators, tool_name):
         for indicator in indicators:
-            detail = indicator_details.get(indicator, {})
-            results.append({
-                "Tool": tool,
-                "Indicator": detail.get("Indicator", ""),
-                "Category": detail.get("Category", ""),
-                "Detail": indicator  # or some other detail from df_details based on indicator
-            })
+            details = df_details[df_details['Indicator'] == indicator]
+            results["Indicator"].append(indicator)
+            results["Unique to"].append(tool_name)
+            results["Category"].append(details["Category"].values[0] if not details.empty else "N/A")
+            results["Metric"].append(details["Metric"].values[0] if not details.empty else "N/A")
+
+    # Add data to the results DataFrame
+    add_details(unique_to_tool1, tool1)
+    add_details(unique_to_tool2, tool2)
+    if tool3 != "None":
+        add_details(unique_to_tool3, tool3)
+    if common_to_all:
+        add_details(common_to_all, "Common to All")
+
+    # Convert results to DataFrame and display
     results_df = pd.DataFrame(results)
-    
+    st.dataframe(results_df)
+
     # Allow downloading as CSV
     csv = results_df.to_csv(index=False)
     b = io.BytesIO(csv.encode())
     st.download_button(label="Download comparison results as CSV", data=b, file_name='comparison_results.csv', mime='text/csv')
-
-# Rest of your Streamlit app code
